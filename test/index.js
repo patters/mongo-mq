f426954ad5e852aaad99253fa93c1e52f0aa6a97
Jason Patterson <Jason.Patterson@dowjones.com>
2015-01-31 11:08:08 -0500

    Adding pub/sub test

--- test/index.js
+++ test/index.js
@@ -13,10 +13,10 @@ describe("# ", function() {
 
 		connection.on('error', function(err) {
 			should.not.exist(err);
-			done();
 		});
 
 		connection.on('ready', function() {
+			connection.close();
 			done();
 		})
 	});
@@ -26,19 +26,43 @@ describe("# ", function() {
 
 		connection.on('error', function(err) {
 			should.not.exist(err);
-			done();
 		});
 
 		connection.on('ready', function() {
 			connection.queue('test', function(err, queue) {
 				should.not.exist(err);
 				should.exist(queue);
+				connection.close();
 				done();
 			});
 		});
 	});
 
 	it('should be able to create a subscription', function(done) {
+		var connection = mq.createConnection(CONNECTION);
+
+		connection.on('error', function(err) {
+			should.not.exist(err);
+		});
+
+		connection.on('ready', function() {
+			connection.queue('test', function(err, queue) {
+				should.not.exist(err);
+				should.exist(queue);
+
+				var sub = queue.subscribe('event', true, function(message) {
+
+				});
+
+				should.exist(sub);
+
+				connection.close();
+				done();
+			});
+		});
+	});
+
+	it('should be able to publish and receive a message', function(done) {
 		var connection = mq.createConnection(CONNECTION);
 
 		connection.on('error', function(err) {
@@ -52,11 +76,13 @@ describe("# ", function() {
 				should.exist(queue);
 
 				var sub = queue.subscribe('event', true, function(message) {
-
+					expect(message).to.equal('hello');
+					done();
 				});
 
 				should.exist(sub);
-				done();
+
+				queue.publish('event', 'hello');
 			});
 		});
 	});
