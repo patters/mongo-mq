24840cfda1f3f2773c53b13a83841cc3d69253d1
Jason Patterson <Jason.Patterson@dowjones.com>
2015-01-31 12:07:40 -0500

    Adding a stress test

--- test/index.js
+++ test/index.js
@@ -215,6 +215,8 @@ describe("# ", function() {
 	});
 
 	it('should be able process messages once with multiple connections', function(done) {
+		this.timeout(15000);
+
 		var pubConnection = mq.createConnection(CONNECTION);
 		var subConnection1 = mq.createConnection(CONNECTION);
 		var subConnection2 = mq.createConnection(CONNECTION);
@@ -252,7 +254,7 @@ describe("# ", function() {
 				callback(null, message + ', 2!');
 			});
 
-			var messages = 500;
+			var messages = 1000;
 			var received = 0;
 			for (var sent = 0; sent < messages; sent++) {
 				queues[0].publish('event', 'hello', function(err, result) {
@@ -305,6 +307,72 @@ describe("# ", function() {
 						connection.close();
 						done();
 					}, 1000);
+				});
+			});
+		});
+	});
+
+	it('should be able to handle a stress test', function(done) {
+		var connection = mq.createConnection(CONNECTION);
+
+		connection.on('error', function(err) {
+			should.not.exist(err);
+			done();
+		});
+
+		connection.on('ready', function() {
+			connection.queue('test', function(err, queue) {
+				should.not.exist(err);
+				should.exist(queue);
+
+				queue.subscribe('main', function(data, callback) {
+					//console.log('in catalog, data=' + data);
+					var manifests = [1, 2, 3, 4, 5, 6];
+
+					async.map(manifests, function(manifest, callback) {
+						queue.publish('top', data + '.' + manifest, function(err, result) {
+							//console.log('catalog got manifest err=' + err + ', result=' + result);
+							callback(err, result);
+						});
+					}, function(err, results) {
+						//console.log('catalog got all manifests, err=' + err + ', results=' + JSON.stringify(results));
+						callback(err, JSON.stringify(results).replace(/"/g, ''));
+					});
+				});
+
+				queue.subscribe('top', function(data, callback) {
+					//console.log('in manifest, data=' + data);
+					queue.publish('middle', data, function(err, result) {
+						//console.log('manifest got issue err=' + err + ', result=' + result);
+						callback(err, result + '.m');
+					});
+				});
+
+				queue.subscribe('middle', function(data, callback) {
+					//console.log('in issue, data=' + data);
+					var pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+					async.map(pages, function(page, callback) {
+						queue.publish('bottom', page, function(err, result) {
+							//console.log('issue got page err=' + err + ', result=' + result);
+							callback(err, result);
+						});
+					}, function(err, results) {
+						results = results.join('');
+						//console.log('issue got all pages, err=' + err + ', results=' + JSON.stringify(results));
+						callback(err, JSON.stringify([data, results]).replace(/"/g, ''));
+					});
+				});
+
+				queue.subscribe('bottom', function(data, callback) {
+					callback(null, 'p' + data);
+				});
+
+				queue.publish({ event: 'main', timeout: 1000 }, 'test', function(err, result) {
+					should.not.exist(err);
+					//console.log(result);
+					expect(result).to.equal('[[test.1,p1p2p3p4p5p6p7p8p9p10].m,[test.2,p1p2p3p4p5p6p7p8p9p10].m,[test.3,p1p2p3p4p5p6p7p8p9p10].m,[test.4,p1p2p3p4p5p6p7p8p9p10].m,[test.5,p1p2p3p4p5p6p7p8p9p10].m,[test.6,p1p2p3p4p5p6p7p8p9p10].m]');
+					connection.close();
+					done();
 				});
 			});
 		});
